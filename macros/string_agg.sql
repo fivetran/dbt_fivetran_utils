c4b463a66a5c8225c7dbf59e24fa91595856496e
fivetran-kristin <kristin@fivetran.com>
2020-10-23 09:33:02 -0700

    add string_agg macro

---
+++ macros/string_agg.sql
@@ -0,0 +1,20 @@
+{% macro string_agg(field_to_agg, delimiter) -%}
+
+{{ adapter.dispatch('string_agg', packages = fivetran_utils._get_utils_namespaces()) (field_to_agg, delimiter) }}
+
+{%- endmacro %}
+
+{% macro default__string_agg(field_to_agg, delimiter) %}
+    string_agg({{ field_to_agg }}, {{ delimiter }})
+
+{% endmacro %}
+
+{% macro snowflake__string_agg(field_to_agg, delimiter) %}
+    listagg({{ field_to_agg }}, {{ delimiter }})
+
+{% endmacro %}
+
+{% macro redshift__string_agg(field_to_agg, delimiter) %}
+    listagg({{ field_to_agg }}, {{ delimiter }})
+
+{% endmacro %}
