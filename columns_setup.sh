35bae3024042013125d8158468070aea7165f649
Dylan Baker <dylang.baker@googlemail.com>
2020-11-16 13:40:40 -0500

    Update columns_setup.sh

--- columns_setup.sh
+++ columns_setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-mkdir -p macros
-mkdir -p models/tmp
-dbt run-operation fivetran_utils.generate_columns_macro --args '{"table_name": "'$1'", "schema_name": "'$2'", "database_name":"'$3'"}' | tail -n +2 >> macros/get_$1_columns.sql
-echo "select *\nfrom {{ var('$1') }}" > models/tmp/$4_$1_tmp.sql
+mkdir -p $1/macros
+mkdir -p $1/models/tmp
+dbt run-operation fivetran_utils.generate_columns_macro --args '{"table_name": "'$5'", "schema_name": "'$4'", "database_name":"'$3'"}' | tail -n +2 > $1/macros/get_$5_columns.sql
+echo "select *\nfrom {{ var('$5') }}" > $1/models/tmp/$2_$5_tmp.sql
 echo "        {{
             fivetran_utils.fill_staging_columns(
-                source_columns=adapter.get_columns_in_relation(ref('$4_$1_tmp')),
-                staging_columns=get_$1_columns()
+                source_columns=adapter.get_columns_in_relation(ref('$2_$5_tmp')),
+                staging_columns=get_$5_columns()
             )
         }}"
