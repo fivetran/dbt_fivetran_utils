4178aa47c41078f48f474d75f5d49a018c2325c3
fivetran-joemarkiewicz <joseph.markiewicz@fivetran.com>
2021-03-04 11:23:00 -0600

    empty var add

---
+++ macros/empty_variable_warning.sql
@@ -0,0 +1,13 @@
+{% macro ticket_field_history_columns_warning(variable, downstream_model) %}
+
+{% if not var(first_value_field) %}
+{{ log(
+    """
+    Warning: You have passed an empty list to the ' {{variable}} '.
+    As a result, you won't see the history of any columns in the ' {{downstream_model}} ' model.
+    """,
+    info=True
+) }}
+{% endif %}
+
+{% endmacro %}
