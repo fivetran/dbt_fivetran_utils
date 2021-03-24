1cd334e2d6173458084c01acd5e1cc3b6ff19ac3
fivetran-joemarkiewicz <joseph.markiewicz@fivetran.com>
2021-03-24 11:00:51 -0500

    quote romval

--- macros/seed_data_helper.sql
+++ macros/seed_data_helper.sql
@@ -1,6 +1,6 @@
 {% macro seed_data_helper(seed_name, warehouse) %}
 
-{% if target.type == "" ~ warehouse ~ "" %}
+{% if target.type == warehouse %}
 {{ return(ref(seed_name ~ "_" ~ warehouse ~ "")) }}
 {% else %}
 {{ return(ref(seed_name)) }}
