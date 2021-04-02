2bcd79545f002c7660e2bc879e38506e8994eb58
Jamie Rodriguez <jamie.rodriguez@fivetran.com>
2021-04-02 16:00:01 -0700

    whitespace

--- macros/pivot_json_extract.sql
+++ macros/pivot_json_extract.sql
@@ -1,8 +1,8 @@
 {% macro pivot_json_extract(string, list_of_properties) %}
 
-{% for property in list_of_properties -%}
-    replace( {{ fivetran_utils.json_extract(string, property) }}, '"', '')
-        as {{ property | replace(' ', '_') | lower }}
+{%- for property in list_of_properties -%}
+
+replace( {{ fivetran_utils.json_extract(string, property) }}, '"', '') as {{ property | replace(' ', '_') | lower }}
 
 {%- if not loop.last -%},{%- endif %}
 {% endfor -%}
