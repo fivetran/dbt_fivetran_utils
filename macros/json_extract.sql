701da08c0e4d3d5cfbac724f4ce508b3adef84d8
fivetran-joemarkiewicz <joseph.markiewicz@fivetran.com>
2021-04-12 14:29:49 -0500

    bq json array

--- macros/json_extract.sql
+++ macros/json_extract.sql
@@ -1,23 +1,32 @@
-{% macro json_extract(string, string_path) -%}
+{% macro json_extract(string, string_path, json_type="scalar") -%}
 
-{{ adapter.dispatch('json_extract', packages = fivetran_utils._get_utils_namespaces()) (string, string_path) }}
+{{ adapter.dispatch('json_extract', packages = fivetran_utils._get_utils_namespaces()) (string, string_path, json_type) }}
 
 {%- endmacro %}
 
-{% macro default__json_extract(string, string_path) %}
+  {% macro default__json_extract(string, string_path, json_type) %}
 
-  json_extract_path_text({{string}}, {{ "'" ~ string_path ~ "'" }} )
- 
-{% endmacro %}
+    json_extract_path_text({{string}}, {{ "'" ~ string_path ~ "'" }} )
+  
+  {% endmacro %}
 
-{% macro bigquery__json_extract(string, string_path) %}
+  {% macro bigquery__json_extract(string, string_path, json_type) %}
 
-  json_extract_scalar({{string}}, {{ "'$." ~ string_path ~ "'" }} )
+  {% if json_type == "array" %}
 
-{% endmacro %}
+    json_extract_array({{string}}, {{ "'$" ~ string_path ~ "'" }} )
+  
+  {% else %}
 
-{% macro postgres__json_extract(string, string_path) %}
+    json_extract_scalar({{string}}, {{ "'$." ~ string_path ~ "'" }} )
+  
+  {% endif %}
+  {% endmacro %}
 
-  {{string}}::json->>{{"'" ~ string_path ~ "'" }}
+  {% macro postgres__json_extract(string, string_path, json_type) %}
+
+    {{string}}::json->>{{"'" ~ string_path ~ "'" }}  
+  
+  {% endmacro %}
 
 {% endmacro %}
