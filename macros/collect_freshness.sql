eb5f24af98d93841d49804203b3665dd1e319eda
Jamie Rodriguez <jamie.rodriguez@fivetran.com>
2021-05-28 09:42:40 -0700

    account for no is_enabled

--- macros/collect_freshness.sql
+++ macros/collect_freshness.sql
@@ -9,7 +9,7 @@
   {%- set enabled_array = [] -%}
   {% for node in graph.sources.values() %}
     {% if node.name == source.name %}
-      {% if node.meta['is_enabled'] is true or node.meta['is_enabled'] is None %}
+      {% if node.meta['is_enabled'] == true or node.meta['is_enabled'] == none %}
         {%- do enabled_array.append(1) -%}
       {% endif %}
     {% endif %}
