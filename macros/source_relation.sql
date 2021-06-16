cf236234128b770825bdfd49ab8a89206f1ca4d4
Dylan Baker <dylang.baker@googlemail.com>
2021-06-16 15:13:05 -0400

    Update source_relation.sql

--- macros/source_relation.sql
+++ macros/source_relation.sql
@@ -9,13 +9,13 @@
 {% if var('union_schemas', none)  %}
 , case
     {% for schema in var('union_schemas') %}
-    when lower(replace(replace(_dbt_source_relation,'"',''),"`",''))) like '%.{{ schema|lower }}.%' then '{{ schema|lower }}'
+    when lower(replace(replace(_dbt_source_relation,'"',''),"`",'')) like '%.{{ schema|lower }}.%' then '{{ schema|lower }}'
     {% endfor %}
   end as source_relation
 {% elif var('union_databases', none) %}
 , case
     {% for database in var('union_databases') %}
-    when lower(replace(replace(_dbt_source_relation,'"',''),"`",''))) like '%{{ database|lower }}.%' then '{{ database|lower }}'
+    when lower(replace(replace(_dbt_source_relation,'"',''),"`",'')) like '%{{ database|lower }}.%' then '{{ database|lower }}'
     {% endfor %}
   end as source_relation
 {% else %}
