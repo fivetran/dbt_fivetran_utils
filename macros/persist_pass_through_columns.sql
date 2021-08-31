d95aac5fc405fe6a7b9c810757bf728643667055
Jamie Rodriguez <65564846+fivetran-jamie@users.noreply.github.com>
2021-08-31 10:56:30 -0400

    Update macros/persist_pass_through_columns.sql
    Co-authored-by: Joe Markiewicz <74217849+fivetran-joemarkiewicz@users.noreply.github.com>

--- macros/persist_pass_through_columns.sql
+++ macros/persist_pass_through_columns.sql
@@ -1,6 +1,6 @@
 {% macro persist_pass_through_columns(pass_through_variable) %}
 
-{% if var(pass_through_variable) %}
+{% if var(pass_through_variable, none) %}
     {% for field in var(pass_through_variable) %}
         , {{ field.alias if field.alias else field.name }}
     {% endfor %}
