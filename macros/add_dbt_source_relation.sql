89b77b77e5669de3e294ff9cf348db404d54904f
Jamie Rodriguez <jamie.rodriguez@fivetran.com>
2021-10-27 12:36:39 -0700

    joe feedback

--- macros/add_dbt_source_relation.sql
+++ macros/add_dbt_source_relation.sql
@@ -1,8 +1,7 @@
 {% macro add_dbt_source_relation() %}
 
 {% if var('union_schemas', none) or var('union_databases', none) %}
-,
-_dbt_source_relation
+, _dbt_source_relation
 {% endif %}
 
 {% endmacro %}
