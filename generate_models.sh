93608193f7ab9e9f8e4898fac55ca8ccfced06ca
fivetran-sheringuyen <94874400+fivetran-sheringuyen@users.noreply.github.com>
2022-01-07 15:19:21 -0800

    separating automation steps into column building then model building

---
+++ generate_models.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+mkdir -p $1/models/tmp
+echo "select * from {{ var('$5') }}" > $1/models/tmp/$2__$5_tmp.sql
+echo "" >> $1/models/$2__$5.sql
+echo "with base as (
+
+    select * 
+    from {{ ref('$2__$5_tmp') }}
+
+),
+
+fields as (
+
+    select
+        {{
+            fivetran_utils.fill_staging_columns(
+                source_columns=adapter.get_columns_in_relation(ref('$2__$5_tmp')),
+                staging_columns=get_$5_columns()
+            )
+        }}
+        
+    from base
+),
+
+final as (
+    
+    select 
+    -- rename here
+    from fields
+)
+
+select * from final" >> $1/models/$2__$5.sql
--- macros/staging_models_automation.sql
+++ macros/staging_models_automation.sql
@@ -4,7 +4,16 @@
 {% set source_schema = ""~ source_schema ~"" %}
 {% set source_database = ""~ source_database ~"" %}
 
-{% set zsh_command = "source dbt_modules/fivetran_utils/columns_setup.sh '../dbt_"""~ package ~"""_source' stg_"""~ package ~""" """~ source_database ~""" """~ source_schema ~""" " %}
+{% set zsh_command = "source dbt_packages/fivetran_utils/generate_columns.sh '../dbt_"""~ package ~"""_source' stg_"""~ package ~""" """~ source_database ~""" """~ source_schema ~""" " %}
+
+{% for t in tables %}
+    {% set help_command = zsh_command + t + " && \n" %}
+
+    {{ log(help_command, info=True) }}
+
+{% endfor %}
+
+{% set zsh_command = "source dbt_packages/fivetran_utils/generate_models.sh '../dbt_"""~ package ~"""_source' stg_"""~ package ~""" """~ source_database ~""" """~ source_schema ~""" " %}
 
 {% for t in tables %}
     {% if t != tables[-1] %}
