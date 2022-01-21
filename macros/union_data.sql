eb4c239d71ea3f56f61974a284c4b6d8ae973471
Deividas J <lizdeika@gmail.com>
2022-01-21 14:17:49 +0200

    Fixed: union_data macro

--- macros/union_data.sql
+++ macros/union_data.sql
@@ -6,11 +6,11 @@
 
 {% macro default__union_data(table_identifier, database_variable, schema_variable, default_database, default_schema, default_variable) %}
 
-{% if var('union_schemas', none) %}
+{% if var('shopify_union_schemas', none) %}
 
     {% set relations = [] %}
 
-    {% for schema in var('union_schemas') %}
+    {% for schema in var('shopify_union_schemas') %}
 
     {% set relation=adapter.get_relation(
         database=var(database_variable, default_database),
@@ -30,11 +30,11 @@
 
     {{ dbt_utils.union_relations(relations) }}
 
-{% elif var('union_databases', none) %}
+{% elif var('shopify_union_databases', none) %}
 
     {% set relations = [] %}
 
-    {% for database in var('union_databases') %}
+    {% for database in var('shopify_union_databases') %}
 
     {% set relation=adapter.get_relation(
         database=database,
