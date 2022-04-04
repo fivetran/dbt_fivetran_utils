c9da60b75a514aff2e960c6e1435ec0337518460
fivetran-reneeli <renee.li@fivetran.com>
2022-04-04 13:30:41 -0400

    formatting and moving macro call to in memory

--- generate_models.sh
+++ generate_models.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-mkdir -p $1/models/tmp #  the $ number signs reference the arguments in the staging_models_automation.sql file line 8
-dbt run-operation fivetran_utils.get_column_names_only --args '{"table_name": "'$5'", "schema_name": "'$4'", "database_name":"'$3'"}' | tail -n +2 > $1/models/tmp/$2__$5_columns.sql 
-# dbt run-operation get_column_names_only --args '{table_name: log, schema_name: fivetran_log, database_name: dbt-package-testing}' >> fivetran_log/models/log__log_temp.sql 
+mkdir -p $1/models/tmp 
 echo "select * from {{ var('$5') }}" > $1/models/tmp/$2__$5_tmp.sql 
 echo "" > $1/models/$2__$5.sql
+
 echo "with base as (
+
     select * 
     from {{ ref('$2__$5_tmp') }}
 ),
+
 fields as (
+
     select
         {{
             fivetran_utils.fill_staging_columns(
@@ -16,16 +18,20 @@ fields as (
                 staging_columns=get_$5_columns()
             )
         }}
-        
     from base
 ),
+
 final as (
     
-    select 
-    " >> $1/models/$2__$5.sql
+    select " >> $1/models/$2__$5.sql
     
-$1/models/tmp/$2__$5_columns.sql >> $1/models/$2__$5.sql
+
+dbt run-operation fivetran_utils.get_column_names_only --args '{"table_name": "'$5'", "schema_name": "'$4'", "database_name":"'$3'"}' | tail -n +2 >> $1/models/$2__$5.sql
     
-echo "from fields
+    
+echo "    from fields
 )
-select * from final" >> $1/models/$2__$5.sql
+
+select *
+from final" >> $1/models/$2__$5.sql
+
