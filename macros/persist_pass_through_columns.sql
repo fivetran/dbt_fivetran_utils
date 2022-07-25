d0dd112950d45b36ade7ac638dd8ffd3d85620ba
Jamie Rodriguez <65564846+fivetran-jamie@users.noreply.github.com>
2022-07-25 16:31:20 -0700

    see if this works

--- macros/persist_pass_through_columns.sql
+++ macros/persist_pass_through_columns.sql
@@ -1,8 +1,8 @@
-{% macro persist_pass_through_columns(pass_through_variable) %}
+{% macro persist_pass_through_columns(pass_through_variable, transform='') %}
 
 {% if var(pass_through_variable, none) %}
     {% for field in var(pass_through_variable) %}
-        , {{ field.alias if field.alias else field.name }}
+        , {{ transform ~ '(' ~ (field.alias if field.alias else field.name) ~ ')' }}
     {% endfor %}
 {% endif %}
 
