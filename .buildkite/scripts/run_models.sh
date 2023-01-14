a2f022f28445beb76698fe262a62fd7eeefcd071
fivetran-sheringuyen <94874400+fivetran-sheringuyen@users.noreply.github.com>
2023-01-13 15:37:46 -0900

    test yq

--- .buildkite/scripts/run_models.sh
+++ .buildkite/scripts/run_models.sh
@@ -4,16 +4,19 @@ set -euo pipefail
 
 apt-get update
 apt-get install libsasl2-dev
+apt-get install yq
 
 python3 -m venv venv
 . venv/bin/activate
 pip install --upgrade pip setuptools
 pip install -r integration_tests/requirements.txt
+pip install yq
 mkdir -p ~/.dbt
 cp integration_tests/ci/sample.profiles.yml ~/.dbt/profiles.yml
 
 db=$1
 echo `pwd`
+yq e -i '.name = "fivetran_utils_test"' dbt_project.yml
 cd integration_tests
 dbt deps
 dbt compile --select tag:ad_reporting
@@ -21,3 +24,6 @@ dbt compile --select tag:zendesk
 dbt compile --select tag:hubspot
 dbt compile --select tag:netsuite
 dbt compile --select tag:jira
+# yq e -i '.name = "fivetran_utils"' dbt_project.yml
+
+
