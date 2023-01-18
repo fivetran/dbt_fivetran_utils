3916163e757b24e407973331fa566bffc2c13ab6
fivetran-sheringuyen <94874400+fivetran-sheringuyen@users.noreply.github.com>
2023-01-18 12:58:23 -0900

    testing packages override

--- .buildkite/scripts/run_models.sh
+++ .buildkite/scripts/run_models.sh
@@ -4,11 +4,9 @@ set -euo pipefail
 
 apt-get update
 apt-get install libsasl2-dev
-apt-get install yq -y
 
 python3 -m venv venv
 . venv/bin/activate
-# brew install yq
 pip install --upgrade pip setuptools
 pip install -r integration_tests/requirements.txt
 mkdir -p ~/.dbt
@@ -16,14 +14,16 @@ cp integration_tests/ci/sample.profiles.yml ~/.dbt/profiles.yml
 
 db=$1
 echo `pwd`
-yq e -i '.name = "fivetran_utils_test"' dbt_project.yml
 cd integration_tests
+dbt deps
+mv packages.yml packages_ft_pkgs.yml
+mv packages_ft_utils_override.yml packages.yml
+## for local testing
+## mv packages.yml packages_ft_utils_override.yml && mv packages_ft_pkgs.yml packages.yml 
 dbt deps
 dbt compile --select tag:ad_reporting
 dbt compile --select tag:zendesk
 dbt compile --select tag:hubspot
 dbt compile --select tag:netsuite
 dbt compile --select tag:jira
-# yq e -i '.name = "fivetran_utils"' dbt_project.yml
-
 
