da2907d9f71d4d249e8b256aa683b8a937987361
fivetran-sheringuyen <94874400+fivetran-sheringuyen@users.noreply.github.com>
2023-01-27 14:12:14 -0900

    removing clutter

--- .buildkite/scripts/run_models.sh
+++ .buildkite/scripts/run_models.sh
@@ -16,13 +16,13 @@ cp integration_tests/ci/sample.profiles.yml ~/.dbt/profiles.yml
 db=$1
 echo `pwd`
 cd integration_tests
-dbt deps
-mv packages.yml packages_ft_pkgs.yml
-mv packages_ft_utils_override.yml packages.yml
-## for local testing
+dbt deps ## Install all packages needed
+mv packages.yml packages_ft_pkgs.yml && mv packages_ft_utils_override.yml packages.yml
+
+## To rename files back to original names for local testing
 ## mv packages.yml packages_ft_utils_override.yml && mv packages_ft_pkgs.yml packages.yml 
 
-dbt deps
+dbt deps ## To override initial fivetran_utils package
 echo 'Compiling Twitter Ads...'
 dbt compile --target "$db" --select tag:twitter_ads
 
