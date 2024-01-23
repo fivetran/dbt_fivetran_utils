a94a5044016337ef480c9f65bb5f6a1fa24b2336
Jamie Rodriguez <65564846+fivetran-jamie@users.noreply.github.com>
2024-01-23 17:08:38 -0300

    try this out

--- macros/remove_prefix_from_columns.sql
+++ macros/remove_prefix_from_columns.sql
@@ -2,7 +2,7 @@
 
         {%- for col in columns if col.name not in exclude -%}
         {%- if col.name[:prefix|length]|lower == prefix -%}
-        {{ col.name }} as {{ col.name[prefix|length:] }}
+        {{ col.name }} as {{ fivetran_utils.wrap_in_quotes(col.name[prefix|length:]) }}
         {%- else -%}
         {{ col.name }}
         {%- endif -%}
