8cc6c43fd3c962584fb15579c427342acd2ff649
Joe Markiewicz <74217849+fivetran-joemarkiewicz@users.noreply.github.com>
2025-12-01 09:09:17 -0600

    Feature/bump dbt to v3 (#154)
    * feature/bump-dbt-to-v3
    
    * integration test package version bump
    
    * mixpanel integration package dep fix
    
    * shopify holistic test removal
    
    * flags move
    
    * standard models command dbt update
    
    * fivetran_utils version print and ad reporting adjustment
    
    * python version bump
    
    * changelog update

--- .buildkite/scripts/run_standard_models.sh
+++ .buildkite/scripts/run_standard_models.sh
@@ -25,8 +25,10 @@ do
     cd dbt_packages/$package/integration_tests/
     dbt deps
     ## Post dbt 1.7.0 we need to edit the package-lock.yml instead of the packages.yml
-    awk '/- package: fivetran\/fivetran_utils/ {print "- local: ../../../../"; skip=1; next} skip && /^  version:/ {skip=0; next} 1' package-lock.yml > temp.yml && mv temp.yml package-lock.yml
+    awk '/name: fivetran_utils/ {print "  - local: ../../../../\n    name: fivetran_utils"; skip=1; next} skip && /^  -/ {skip=0} !skip' package-lock.yml > temp.yml && mv temp.yml package-lock.yml
     dbt deps
+    fivetran_utils_version=$(grep "^version:" dbt_packages/fivetran_utils/dbt_project.yml | awk '{print $2}')
+    echo -e "\nUsing fivetran_utils version: "$fivetran_utils_version"\n"
     if [ "$package" = "linkedin" ]; then
         value_to_replace=$(grep ""$package"_ads_schema:" dbt_project.yml | awk '{ print $2 }')
         perl -i -pe "s/(schema: |dataset: ).*/\1$value_to_replace/" ~/.dbt/profiles.yml
@@ -49,7 +51,11 @@ do
         perl -i -pe "s/(schema: |dataset: ).*/\1$value_to_replace/" ~/.dbt/profiles.yml
     fi
     dbt seed --target "$db"
-    dbt run --target "$db"
+    if [ "$package" = "ad_reporting" ]; then
+        dbt run --target "$db" --vars '{ad_reporting__facebook_ads_enabled: true, facebook_ads__using_demographics_country: true, facebook_ads__using_demographics_region: true, ad_reporting__google_ads_enabled: true, ad_reporting__amazon_ads_enabled: false, ad_reporting__apple_search_ads_enabled: false, ad_reporting__linkedin_ads_enabled: true, ad_reporting__microsoft_ads_enabled: false, ad_reporting__pinterest_ads_enabled: false, ad_reporting__reddit_ads_enabled: false, ad_reporting__snapchat_ads_enabled: false, ad_reporting__tiktok_ads_enabled: false, ad_reporting__twitter_ads_enabled: false}'
+    else
+        dbt run --target "$db"
+    fi
     dbt run-operation fivetran_utils.drop_schemas_automation --target "$db"
     cd ../../../
 done
