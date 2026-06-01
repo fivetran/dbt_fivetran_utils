d261b1277caacf4d8acf7029674694dd5fdd1de9
Catherine Fritz <111930712+fivetran-catfritz@users.noreply.github.com>
2026-06-01 18:17:49 -0500

    update fill staging cols

--- macros/fill_staging_columns.sql
+++ macros/fill_staging_columns.sql
@@ -1,35 +1,23 @@
 {% macro fill_staging_columns(source_columns, staging_columns) -%}
 
 {%- set source_column_names = source_columns|map(attribute='name')|map('lower')|list -%}
+{%- set using_mdls = var('using_fivetran_mdls', false) %}
 
 {%- for column in staging_columns %}
-    {% if column.name|lower in source_column_names -%}
-        {{ fivetran_utils.quote_column(column) }} as 
-        {%- if 'alias' in column %} {{ column.alias }} {% else %} {{ fivetran_utils.quote_column(column) }} {%- endif -%}
+    {%- if column.name|lower in source_column_names -%}
+        {{ adapter.quote(column.name) if using_mdls else fivetran_utils.quote_column(column) }}
     {%- else -%}
         cast(null as {{ column.datatype }})
-        {%- if 'alias' in column %} as {{ column.alias }} {% else %} as {{ fivetran_utils.quote_column(column) }} {% endif -%}
     {%- endif -%}
-    {%- if not loop.last -%} , {% endif -%}
-{% endfor %}
+    as {{ column.alias if 'alias' in column else fivetran_utils.quote_column(column) }}{{ ',' if not loop.last }}
 
-{% endmacro %}
+{%- endfor %}
+{%- endmacro %}
 
-
-{% macro quote_column(column) %}
-    {% if 'quote' in column %}
-        {% if column.quote %}
-            {% if target.type in ('bigquery', 'spark', 'databricks') %}
-            `{{ column.name }}`
-            {% elif target.type == 'snowflake' %}
-            "{{ column.name | upper }}"
-            {% else %}
-            "{{ column.name }}"
-            {% endif %}
-        {% else %}
+{%- macro quote_column(column) %}
+    {%- if ('quote' in column and column.quote) or using_mdls %}
+        {{ adapter.quote(column.name|upper if target.type == 'snowflake' else column.name) }}
+    {%- else %}
         {{ column.name }}
-        {% endif %}
-    {% else %}
-    {{ column.name }}
-    {% endif %}
-{% endmacro %}
+    {%- endif %}
+{%- endmacro %}
