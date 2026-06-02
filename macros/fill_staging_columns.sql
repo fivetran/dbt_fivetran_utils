a7a47a2b1578e2ca2be572e8987c204cf144eb64
Catherine Fritz <111930712+fivetran-catfritz@users.noreply.github.com>
2026-06-02 16:49:23 -0500

    whitespace control

--- macros/fill_staging_columns.sql
+++ macros/fill_staging_columns.sql
@@ -1,47 +1,38 @@
-{% macro fill_staging_columns(source_columns, staging_columns) -%}
+{% macro fill_staging_columns(source_columns, staging_columns) %}
 
 {%- set source_column_names = source_columns|map(attribute='name')|map('lower')|list -%}
-{%- set using_source_casing = var('using_deliberate_quoting', true) -%} --var('fivetran_using_source_casing', false) -%}
+{%- set using_source_casing = var('using_deliberate_quoting', true) -%}
 
 {%- for column in staging_columns %}
-
-    {%- if column.name|lower in source_column_names -%}
-        
-        {% if using_source_casing -%}
-        {%- set column_alias = column.alias if 'alias' in column else column.name -%}
-        {{ adapter.quote(column.name) }} as {{ adapter.quote(column_alias|upper if target.warehouse == 'snowflake' else column_alias) }} 
-        
+    {%- if column.name|lower in source_column_names %}
+        {%- if using_source_casing %}
+            {%- set column_alias = column.alias if 'alias' in column else column.name %}
+            {{ adapter.quote(column.name) }} as {{ adapter.quote(column_alias|upper if target.warehouse == 'snowflake' else column_alias) }}
         {%- else %}
-        {{ fivetran_utils.quote_column(column) }} as
-        {%- if 'alias' in column %} {{ column.alias }} {% else %} {{ fivetran_utils.quote_column(column) }} {%- endif -%}
-        
+            {{ fivetran_utils.quote_column(column) }} as
+            {%- if 'alias' in column %} {{ column.alias }}{% else %} {{ fivetran_utils.quote_column(column) }}{% endif %}
         {%- endif %}
-    
-    {%- else -%}
-        cast(null as {{ column.datatype }}) as
-        {%- if 'alias' in column %} {{ column.alias }} {% else %} {{ fivetran_utils.quote_column(column) }} {% endif -%}
-
-    {%- endif -%}{{ ',' if not loop.last }}
-
+    {%- else %}
+            cast(null as {{ column.datatype }}) as
+            {%- if 'alias' in column %} {{ column.alias }}{% else %} {{ fivetran_utils.quote_column(column) }}{% endif %}
+    {%- endif %}{{ ',' if not loop.last }}
 {%- endfor %}
 
-{% endmacro %}
+{%- endmacro %}
 
 
 {% macro quote_column(column) %}
-    {% if 'quote' in column %}
-        {% if column.quote %}
-            {% if target.type in ('bigquery', 'spark', 'databricks') %}
+    {%- if 'quote' in column %}
+        {%- if column.quote %}
+            {%- if target.type in ('bigquery', 'spark', 'databricks') %}
             `{{ column.name }}`
-            {% elif target.type == 'snowflake' %}
+            {%- elif target.type == 'snowflake' %}
             "{{ column.name | upper }}"
-            {% else %}
+            {%- else %}
             "{{ column.name }}"
-            {% endif %}
-        {% else %}
-        {{ column.name }}
-        {% endif %}
-    {% else %}
-    {{ column.name }}
-    {% endif %}
-{% endmacro %}
+            {%- endif %}
+        {%- else %}{{ column.name }}
+        {%- endif %}
+    {%- else %}{{ column.name }}
+    {%- endif %}
+{%- endmacro %}
