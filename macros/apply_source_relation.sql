1e3f66ffe44c25041b24fbd53edc90e0d1f38d20
Catherine Fritz <111930712+fivetran-catfritz@users.noreply.github.com>
2026-06-01 19:13:33 -0500

    source relation

--- macros/apply_source_relation.sql
+++
@@ -1,7 +0,0 @@
-{% macro apply_source_relation() -%}
-{{ adapter.dispatch('apply_source_relation', 'fivetran_utils') () }}
-{%- endmacro %}
-
-{% macro default__apply_source_relation() -%}
-_dbt_source_relation as source_relation
-{%- endmacro %}
