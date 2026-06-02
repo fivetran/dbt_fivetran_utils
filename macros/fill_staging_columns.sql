16ae6871af4f1ea33e7dd1563344e707404d4230
Catherine Fritz <111930712+fivetran-catfritz@users.noreply.github.com>
2026-06-01 21:17:47 -0500

    fill staging

--- macros/fill_staging_columns.sql
+++ macros/fill_staging_columns.sql
@@ -1,32 +1,26 @@
 {% macro fill_staging_columns(source_columns, staging_columns) -%}
 
-{%- set using_source_casing = var('fivetran_using_source_casing', false) %}
-{%- set source_column_names = source_columns|map(attribute='name')|map('lower')|list -%}
-{%- do staging_columns.append({'name': 'source_relation', 'datatype': dbt.type_string()})   %}
+{%- set using_source_casing = var('fivetran_using_source_casing', false) -%}
+{%- set source_column_names = source_columns | map(attribute='name') | map('lower') | list -%}
+{%- do staging_columns.append({'name': 'source_relation', 'datatype': dbt.type_string()}) -%}
 
 {%- for column in staging_columns %}
-    {%- set column_name = column.name %}
-    {%- set column_alias = column.alias if 'alias' in column else column.name %}
+    {%- set column_name = column.name -%}
+    {%- set column_alias = column.alias if 'alias' in column else column_name -%}
+    {%- set quote_columns = ('quote' in column and column.quote) or using_source_casing -%}
 
-    {%- if column.name|lower in source_column_names %}
-        -- Preserve the original casing of the column name from the source but uppercase the alias in Snowflake since Snowflake folds unquoted identifiers to uppercase.
-        {% if using_source_casing %} 
-        {{ adapter.quote(column_name) }} -- always quote but don't change case
-        {%- else  %}
-        {{ fivetran_utils.quote_column(column, column_name) }}
-        {%- endif %}
+    {%- set adjusted_column_name_casing = column_name|upper if target.warehouse == 'snowflake' and not using_source_casing else column_name -%}
+    {%- set adjusted_alias_casing = column_alias|upper if target.warehouse == 'snowflake' else column_alias -%}
+
+    {%- if column_name|lower in source_column_names -%}
+        {%- set rendered_column_name = adapter.quote(adjusted_column_name_casing) if quote_columns else adjusted_column_name_casing -%}
     {%- else %}
-        cast(null as {{ column.datatype }})
-    {%- endif %}
-    as {{ fivetran_utils.quote_column(column, column_alias) }},
+        {%- set rendered_column_name = 'cast(null as ' ~ column.datatype ~ ')' -%}
+    {%- endif -%}
+
+    {%- set rendered_alias = adapter.quote(adjusted_alias_casing) if quote_columns else adjusted_alias_casing -%}
+    
+    {{ rendered_column_name }} as {{ rendered_alias }}{{',' if not loop.last }}
+
 {%- endfor %}
 {%- endmacro %}
-
-
-{% macro quote_column(column, column_name=column.name) %}
-    {% if ('quote' in column and column.quote) or using_source_casing %}
-        {{ adapter.quote(column_name|upper if target.type == 'snowflake' else column_name) }}
-    {% else %}
-        {{ column_name }}
-    {% endif %}
-{% endmacro %}
