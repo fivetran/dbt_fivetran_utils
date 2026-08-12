6669673ca2a5a389a09a5c335eedc9524ae20d0c
Jamie Rodriguez <jamie.rodriguez@fivetran.com>
2026-08-12 10:04:33 -0400

    catherine feedback

--- macros/percentile.sql
+++ macros/percentile.sql
@@ -56,6 +56,10 @@
 
 {% macro duckdb__percentile(percentile_field, partition_field, percent) %}
 
-    quantile_cont({{ percentile_field }}, {{ percent }}) over (partition by {{ partition_field }})
+    quantile_cont(
+        {{ percentile_field }},
+        {{ percent }})
+        over (partition by {{ partition_field }}
+        )
 
 {% endmacro %}
